b3118549b64f7f3ee402400463cf26c3ba4de0a4
Kelly Diang'a <91629718+KileyDA@users.noreply.github.com>
2026-07-01 10:51:50 +0100

    feat: add LED, buzzer and acknowledge button tests

--- firmware/stm32-f446re/smart_monitoring_system_STM32/Core/Src/main.c
+++ firmware/stm32-f446re/smart_monitoring_system_STM32/Core/Src/main.c
@@ -44,12 +44,15 @@
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
 
+TIM_HandleTypeDef htim3;
+
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 uint32_t ldr_adc_value =0;
 uint32_t ldr_voltage_mv = 0;
 GPIO_PinState pir_state;
+GPIO_PinState ack_button_state;
 char uart_message[100];
 
 /* USER CODE END PV */
@@ -59,6 +62,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_TIM3_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -99,9 +103,14 @@ int main(void)
   MX_GPIO_Init();
   MX_ADC1_Init();
   MX_USART2_UART_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
-  char start_msg[] = "STM32 LDR and PIR sensor test started\r\n";
+  char start_msg[] = "STM32 LDR, PIR, LED, buzzer and acknowledge button test started\r\n";
   HAL_UART_Transmit(&huart2, (uint8_t*)start_msg, strlen(start_msg), HAL_MAX_DELAY);
+
+  /* Start PWM for Security Light LED on TIM3 Channel 1 */
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -142,11 +151,55 @@ int main(void)
 	    }
 
 	    HAL_UART_Transmit(&huart2,
-	  		  (uint8_t*)uart_message,
-	  		  strlen(uart_message),
-	  		  HAL_MAX_DELAY);
+						  (uint8_t*)uart_message,
+						  strlen(uart_message),
+						  HAL_MAX_DELAY);
+
+	    /* Read Reset/Acknowledge Button */
+	    ack_button_state = HAL_GPIO_ReadPin(ACK_BUTTON_GPIO_Port, ACK_BUTTON_Pin);
+
+	    if (ack_button_state == GPIO_PIN_RESET)
+	    {
+	        snprintf(uart_message, sizeof(uart_message), "Acknowledge Button: PRESSED\r\n");
+	    }
+	    else
+	    {
+	        snprintf(uart_message, sizeof(uart_message), "Acknowledge Button: NOT PRESSED\r\n");
+	    }
+
+	    HAL_UART_Transmit(&huart2,
+	                      (uint8_t*)uart_message,
+	                      strlen(uart_message),
+	                      HAL_MAX_DELAY);
+
+	    /* Test Security Light LED brightness using PWM */
+	    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, 200);  // Low brightness
+
+	    /* Test Warning LED ON/OFF */
+	    HAL_GPIO_WritePin(WARNING_LED_GPIO_Port, WARNING_LED_Pin, GPIO_PIN_SET);
+	    HAL_Delay(500);
+
+	    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, 999);  // High brightness
+	    HAL_GPIO_WritePin(WARNING_LED_GPIO_Port, WARNING_LED_Pin, GPIO_PIN_RESET);
 	  HAL_Delay(500);
+
+	  /* Test active buzzer */
+	  HAL_GPIO_WritePin(ACTIVE_BUZZER_GPIO_Port, ACTIVE_BUZZER_Pin, GPIO_PIN_SET);
+
+	  snprintf(uart_message, sizeof(uart_message), "Active Buzzer: ON\r\n");
+	  HAL_UART_Transmit(&huart2, (uint8_t*)uart_message, strlen(uart_message), HAL_MAX_DELAY);
+
+	  HAL_Delay(300);
+
+	  HAL_GPIO_WritePin(ACTIVE_BUZZER_GPIO_Port, ACTIVE_BUZZER_Pin, GPIO_PIN_RESET);
+
+	  snprintf(uart_message, sizeof(uart_message), "Active Buzzer: OFF\r\n");
+	  HAL_UART_Transmit(&huart2, (uint8_t*)uart_message, strlen(uart_message), HAL_MAX_DELAY);
+
+	  HAL_Delay(700);
+
   }
+
   /* USER CODE END 3 */
 }
 
@@ -250,6 +303,55 @@ static void MX_ADC1_Init(void)
 }
 
 /**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 83;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 999;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+  HAL_TIM_MspPostInit(&htim3);
+
+}
+
+/**
   * @brief USART2 Initialization Function
   * @param None
   * @retval None
@@ -301,7 +403,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LD2_Pin|WARNING_LED_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(ACTIVE_BUZZER_GPIO_Port, ACTIVE_BUZZER_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -315,12 +420,25 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(PIR_MOTION_INPUT_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
+  /*Configure GPIO pins : LD2_Pin WARNING_LED_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin|WARNING_LED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : ACK_BUTTON_Pin */
+  GPIO_InitStruct.Pin = ACK_BUTTON_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(ACK_BUTTON_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : ACTIVE_BUZZER_Pin */
+  GPIO_InitStruct.Pin = ACTIVE_BUZZER_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(ACTIVE_BUZZER_GPIO_Port, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
